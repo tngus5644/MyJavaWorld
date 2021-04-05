77a227351697becbc2d747c191b3c65bdaec236c
tngus5644 <tngus5644@kakao.com>
2021-04-05 21:44:33 +0900

    Intent

---
+++ app/src/main/java/com/example/myjavaworld/SubActivity.java
@@ -0,0 +1,33 @@
+package com.example.myjavaworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView tv_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        tv_sub = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        tv_sub.setText(str);
+
+
+
+
+
+
+
+    }
+
+}
